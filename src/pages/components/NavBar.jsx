b1bc2c95cb4cf42c8410ae0ae2ada78d9a40282e
Projeckt Bits <118530911+ItzSouraseez@users.noreply.github.com>
2023-04-22 10:58:19 +0530

    0.0.2.a

--- src/pages/components/NavBar.jsx
+++ src/pages/components/NavBar.jsx
@@ -8,12 +8,12 @@ function RotateArrow() {
   const MenuButton = document.getElementById("MenuButton");
   const Arrow = document.getElementById("Arrow");
   if (State == 0) {
-    Arrow.style.cssText = "transform: rotate(180deg); transition: all 100ms ease-in-out";
-    
+    Arrow.style.cssText =
+      "transform: rotate(180deg); transition: all 100ms ease-in-out";
     State = 1;
   } else {
-    Arrow.style.cssText = "transform: rotate(0deg); transition: all 100ms ease-in-out";
-    
+    Arrow.style.cssText =
+      "transform: rotate(0deg); transition: all 100ms ease-in-out";
     State = 0;
   }
 }
@@ -41,6 +41,28 @@ export default function NavBar() {
                 priority
               />
             </button>
+            <ul className={Styles.NavUl}>
+              <li className={Styles.NavLi}>
+                <button className={Styles.NavLiBtn}>
+                  Home
+                </button>
+                <button className={Styles.NavLiBtn}>
+                  PetLog
+                </button>
+                <button className={Styles.NavLiBtn}>
+                  PetShoppe
+                </button>
+                <button className={Styles.NavLiBtn}>
+                  Support Us
+                </button>
+                <button className={Styles.NavLiBtn}>
+                  About Us
+                </button>
+                <button className={Styles.NavLiBtn}>
+                  Contact Us
+                </button>
+              </li>
+            </ul>
           </nav>
         </div>
       </header>
